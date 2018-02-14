09ceb07d31e95481bfccedca8f4bd0c808e8492c
Daniel Turanov <30848754+ldtek@users.noreply.github.com>
2018-02-14 13:13:40 -0600

    Add files via upload

---
+++ Archiving.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression; 
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Compression3
+{
+    class Archiving
+    {
+        public void Compress(FileInfo FileToCompress,ListBox List)
+        {
+            using (FileStream originalFileStream = FileToCompress.OpenRead())
+            {
+               if ((File.GetAttributes(FileToCompress.FullName) & FileAttributes.Hidden) != FileAttributes.Hidden & FileToCompress.Extension != ".gz")
+                {
+                    using (FileStream compressedFileStream = File.Create(FileToCompress.FullName + ".gz"))
+                    {
+                        using (GZipStream compressionStream = new GZipStream(compressedFileStream, CompressionMode.Compress))
+                        {
+                            originalFileStream.CopyTo(compressionStream);
+                            List.Items.Add("Compressed " + FileToCompress.Name + " From " + FileToCompress.Length + " to " + compressedFileStream.Length);
+                        }
+                    }
+                }
+            }
+        }
+
+        public void Extract(FileInfo FileToDecompress)
+        {
+            using (FileStream originalStream = FileToDecompress.OpenRead())
+            {
+                string currentFileName = FileToDecompress.FullName;
+                string newFileName = currentFileName.Remove(currentFileName.Length - FileToDecompress.Extension.Length);
+                using (FileStream DecompressedFileStream = File.Create(newFileName))
+                {
+                    using (GZipStream DecompressionStream = new GZipStream(originalStream,CompressionMode.Decompress))
+                    {
+                         if (FileToDecompress.Extension ==".gz") 
+                        {
+                            DecompressionStream.CopyTo(DecompressedFileStream);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
